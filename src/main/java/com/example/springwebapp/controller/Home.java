e6f871fca383a3ea27624bf2f8a4d0a49fdb796b
Sergey Khudanin <Seregahudanin@yandex.ru>
2021-09-30 00:11:33 +0500

    Tried Bootstrap

--- src/main/java/com/example/springwebapp/controller/Home.java
+++ src/main/java/com/example/springwebapp/controller/Home.java
@@ -16,19 +16,28 @@ import java.util.Date;
 @Controller
 public class Home {
 
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     public String home() {
         return "index";
-    }
+    }*/
 
-    @RequestMapping("/home-with-session")
+    //@RequestMapping("/home-with-session")
+    @RequestMapping("/")
     public String homeWithSession(Model model, HttpSession session) {
         String sid = session.getId();
         model.addAttribute("sid", sid);
 
         ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product("Old product", 1, new Date()));
-        products.add(new Product("New product", 2, new Date()));
+        products.add(new Product("Product_1", 1, new Date()));
+        products.add(new Product("Product_2", 2, new Date()));
+        products.add(new Product("Product_3", 3, new Date()));
+        products.add(new Product("Product_4", 4, new Date()));
+        products.add(new Product("Product_5", 5, new Date()));
+        products.add(new Product("Product_6", 6, new Date()));
+        products.add(new Product("Product_7", 7, new Date()));
+        products.add(new Product("Product_8", 8, new Date()));
+        products.add(new Product("Product_9", 9, new Date()));
+        products.add(new Product("Product_10", 10, new Date()));
         model.addAttribute("products", products);
         return "index";
     }
